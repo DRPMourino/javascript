ce2273717622f578d25ddc14f7e74c2e359eba00
DRPMourino <danrob992010@hotmail.com>
2023-12-07 15:29:59 -0300

    Aula 12

---
+++ Aula 13. Java S/ambiente.js
@@ -0,0 +1,15 @@
+//Teste lógico no início
+/* var c = 1
+while (c <= 500){
+    console.log (`passo ${c}`)
+    c++
+    } */
+
+
+
+//Teste lógico no Final
+    var c = 1
+    do {
+        console.log (`passo ${c}`)
+        c++
+    } while (c <= 6) 
